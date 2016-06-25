1e6dedf559307be025affbc0652dd9b1035528e1
Matthias-Oppacher <matthias.oppacher+github@googlemail.com>
2016-06-25 11:23:14 +0200

    Leeres Web API Projekt angelegt

--- ArtikelVerwaltung/ArtikelVerwaltung.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ArtikelVerwaltung.API.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- ArtikelVerwaltung/ArtikelVerwaltung.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ArtikelVerwaltung.API.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
