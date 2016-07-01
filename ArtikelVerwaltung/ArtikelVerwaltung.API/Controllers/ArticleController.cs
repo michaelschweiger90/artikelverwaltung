1d993962f7a09529362f3d252bc9f3e4bcb6ee3c
michaelschweiger90 <m.schweiger90@web.de>
2016-07-01 14:22:26 +0200

    article controller update

--- ArtikelVerwaltung/ArtikelVerwaltung.API/Controllers/ArticleController.cs
+++ ArtikelVerwaltung/ArtikelVerwaltung.API/Controllers/ArticleController.cs
@@ -15,14 +15,47 @@ namespace ArtikelVerwaltung.API.Controllers
 		{
 		}
 
-		[Route("~/api/v1/articles")]
+		[Route("~/api/v1/category/{id:int}/articles")]
 		[HttpGet]
-		public IHttpActionResult getAllArticle()
+		public IHttpActionResult getAllArticle(int id)
 		{
-			List<ArticleDTO> articles = ModelFactory.Create(ArticleRepository.GetAllArticle());
+			List<ArticleDTO> articles = ModelFactory.Create(CategoryRepository.GetArticleById(id));
 
 			return Ok(articles);
 		}
 
-	}
+        [Route("~/api/v1/category/{cId:int}/article/{aId:int}")]
+        [HttpGet]
+        public IHttpActionResult getArticle(int cId, int aId)
+        {
+            //TODO
+
+            return;
+        }
+
+        [Route("~/api/v1/category/{id:int}/articles")]
+        [HttpPost]
+        public IHttpActionResult createArticle(int id)
+        {
+            //TODO
+            return;
+        }
+
+        [Route("~/api/v1/category/{cId:int}/articles{aId:int}")]
+        [HttpPut]
+        public IHttpActionResult updateArticle(int cId, int aId)
+        {
+            //TODO
+            return;
+        }
+
+        [Route("~/api/v1/category/{cId:int}/article{aId::int}")]
+        [HttpDelete]
+        public IHttpActionResult deleteArticle(int cId, int aId)
+        {
+            //TODO
+            return;
+        }
+
+    }
 }
