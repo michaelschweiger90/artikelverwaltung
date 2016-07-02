3fcb6e91b3687534815991c567be07c80dc6ccd8
michaelschweiger90 <m.schweiger90@web.de>
2016-07-02 14:16:44 +0200

    category update

--- ArtikelVerwaltung/ArtikelVerwaltung.Client/app/controllers/article.js
+++ ArtikelVerwaltung/ArtikelVerwaltung.Client/app/controllers/article.js
@@ -2,6 +2,29 @@
     '$rootScope', '$scope', 'Category',
     function ($rootScope, $scope, Category) {
 
+        $scope.menuOptions = [
+            ['Kategorie löschen', function ($itemScope) {
+                var categorie = $itemScope.category;
+                categorie.$delete();
+            }],
+            ['Kategorie umbennen', function ($itemScope) {
+                var categorie = $itemScope.category;
+                /*
+                Category.deleteArticleFromCategory(article, $stateParams.id, function (article) {
+                    $scope.article = article;
+                });
+                */
+            }],
+            ['Kategorie hinzufügen', function ($itemScope) {
+                var categorie = $itemScope.category;
+                /*
+                Category.deleteArticleFromCategory(article, $stateParams.id, function (article) {
+                    $scope.article = article;
+                });
+                */
+            }]
+        ];
+
         Category.get(function(categories){
             $scope.categories = categories;
         });
