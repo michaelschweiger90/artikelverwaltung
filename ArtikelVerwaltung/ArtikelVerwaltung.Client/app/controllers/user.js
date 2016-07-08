048c516cab6e4686b75099ab214d30c602f4d885
Veli Döngelci <dongelci@outlook.com>
2016-07-08 03:31:41 +0200

    - app.js will be called after successful login.  register, login and forgot password are independent from app.js - added material icons - som fixes

---
+++ ArtikelVerwaltung/ArtikelVerwaltung.Client/app/controllers/user.js
@@ -0,0 +1,30 @@
+﻿app.controller('LoginCtrl', [
+    '$localStorage', '$rootScope', '$scope', '$state', 'AuthService',
+    function ($localStorage, $rootScope, $scope, $state, AuthService)
+    {
+        $scope.user = null;
+        $scope.dataLoading = false;
+
+        $scope.doLogin = function () {
+            $scope.dataLoading = true;
+
+            AuthService.doLogin($scope.user).$promise.then(
+                function (data)
+                {
+                    var user = {};
+                    user.name = data.name;
+                    user.mailAddress = data.mailAddress;
+                    user.isAdmin = data.isAdmin;
+                    user.id = data.id;
+                    user.authToken = data.token,
+                    $localStorage.user = user;
+
+                    $state.go('app.article.list');
+                }, function (data)
+                {
+                    $scope.dataLoading = false;
+                }
+            );
+        };
+    }
+]);
